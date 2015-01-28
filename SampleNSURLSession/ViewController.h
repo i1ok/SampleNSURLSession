38fcd67a782911e72bb073735c8e69bf1a2755ff
Keita Ito <gi1okt@gmail.com>
2015-01-28 00:13:40 -0800

    Initial Commit

---
+++ SampleNSURLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleNSURLSession
+//
+//  Created by Keita on 1/28/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleNSURLSessionTests/SampleNSURLSessionTests.m
@@ -0,0 +1,40 @@
+//
+//  SampleNSURLSessionTests.m
+//  SampleNSURLSessionTests
+//
+//  Created by Keita on 1/28/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SampleNSURLSessionTests : XCTestCase
+
+@end
+
+@implementation SampleNSURLSessionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
